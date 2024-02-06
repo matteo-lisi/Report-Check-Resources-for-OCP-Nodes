77991c28bcacdf3654ee325df35baa5c1a8281bc
Matteo Lisi <68588860+matteo-lisi@users.noreply.github.com>
2024-02-06 17:52:18 +0100

    Add date on CSV file

--- check_nodes_resources.sh
+++ check_nodes_resources.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Version: 2.0
+# Version: 2.1
 #
 # Author Matteo Lisi
 #
 
 # Allocatable and Allocated Resources Report of all NODES
-LOG_FILE="nodes_resources.csv"
+RUN_DATE=$(date +%Y%m%d-%H%M)
+LOG_FILE="nodes_resources-${RUN_DATE}.csv"
 echo "NodeName;Pods;Unschedulable;TotalCPU;TotalMemory;CPU_Requests;CPU_Requests(%);CPU_Limits;CPU_Limits(%);Memory_Requests;Memory_Requests(%);Memory_Limits;Memory_Limits(%)" | tee "${LOG_FILE}" | column -t -s ';' 
 for node in $(oc get nodes -o name | cut -d/ -f2); do
   echo -ne "Process the Node: $node\r"
@@ -14,3 +15,7 @@ for node in $(oc get nodes -o name | cut -d/ -f2); do
     awk -v NODENAME="$node" '{print NODENAME";"$9";"$2";"$4";"$6";"$13";"$14";"$15";"$16";"$18";"$19";"$20";"$21}' | tee -a "${LOG_FILE}" | column -t -s ';' 
 done
 column -t -s ';' "${LOG_FILE}"
+
+echo -e "\n######################################################"
+echo "# CSV file name: ${LOG_FILE}"
+echo "######################################################"
